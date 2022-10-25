ba04e499d3a36ba309a1df8fc9e6778ddaf9ad81
Elite Carlson <elitecarlson@gmail.com>
2022-10-25 02:54:09 +0200

    file structure clean up

--- 1st Term/Works/Firebase/src/App.jsx
+++ 1st Term/Works/Firebase/src/App.jsx
@@ -3,17 +3,16 @@ import "../src/Assets/css/app.css";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import Account from "./components/account";
-import PageNotFound from "./components/404";
-import Home from "./components/index";
+// import PageNotFound from "./components/404";
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Account/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/signup" element={<Signup/>}></Route>
-      <Route path="/:id" element={<Account/>}></Route>
-      {/* <Route path="*" element={<PageNotFound/>}></Route> */}
+      {/* <Route path="/:id" element={<Account/>}></Route>
+      <Route path="*" element={<PageNotFound/>}></Route> */}
     </Routes>
     </>
   );
