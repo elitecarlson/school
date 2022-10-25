e85979863129d0d20586ce0e492e14fe039597d4
Elite Carlson <elitecarlson@gmail.com>
2022-10-25 02:51:45 +0200

    Primary files

--- 1st Term/Works/Firebase/src/App.jsx
+++ 1st Term/Works/Firebase/src/App.jsx
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "../src/Assets/css/app.css";
-// import icon from "../src/Assets/images/icon.jpg";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Account from "./components/account";
+import PageNotFound from "./components/404";
+import Home from "./components/index";
 function App() {
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/:id" element={<Account/>}></Route>
+      {/* <Route path="*" element={<PageNotFound/>}></Route> */}
+    </Routes>
+    </>
   );
 }
 
