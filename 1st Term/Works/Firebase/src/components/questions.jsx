6d7fa7e1c527acf90ee548cd172e5e191344c1e0
Elite Carlson <elitecarlson@gmail.com>
2022-11-09 01:38:42 +0200

    video player stable

--- 1st Term/Works/Firebase/src/components/questions.jsx
+++ 1st Term/Works/Firebase/src/components/questions.jsx
@@ -6,6 +6,7 @@ import { useRef } from "react";
 function Questions(){
     const[btnVis, setBtnVis] = useState(true);
     const[mute, setMute] = useState(false);
+    const[paused, setPaused] = useState(false);
     const vidRef = useRef();
     const vidProgressBar = useRef();
     function playVid(){
@@ -13,8 +14,14 @@ function Questions(){
         vidRef.current.play();
     }
     function pauseVid(){
-        setBtnVis(true);
-        vidRef.current.pause();
+        if (vidRef.current.paused == false) {
+            vidRef.current.pause();
+            setPaused(true);
+        } else {
+            setBtnVis(false);
+            vidRef.current.play();
+            setPaused(false);
+        }
     }
     function muteUnmute(){
         if (vidRef.current.muted == false) {
@@ -41,18 +48,21 @@ function Questions(){
     }
     return(
         <>
-        <div className={css.vidCont}>
-            <div className={btnVis ? css.Hidden : css.topNav}>
-                <div className={css.pause} onClick={pauseVid}></div>
-                <div className={mute ? css.mute : css.volume} onClick={muteUnmute}></div>
+        <div className={css.Container}>
+            <div className={css.vidCont}>
+                <div className={btnVis ? css.Hidden : css.topNav}>
+                    <div className={paused ? css.play : css.pause} onClick={pauseVid}></div>
+                    <div className={mute ? css.mute : css.volume} onClick={muteUnmute}></div>
+                </div>
+            <video src={vid} className={css.vid} ref={vidRef} onTimeUpdate={timeUpdate}></video>
+            <div className={btnVis ? css.playBtn : css.Hidden} onClick={playVid}>
+                <div className={css.playIcon}></div>
             </div>
-        <video src={vid} className={css.vid} ref={vidRef} onTimeUpdate={timeUpdate}></video>
-        <div className={btnVis ? css.playBtn : css.Hidden} onClick={playVid}>
-            <div className={css.playIcon}></div>
-        </div>
-        <div className={btnVis ? css.Hidden : css.rangeCont}>
-            <input type="range" class="progressbar" className={css.range} ref={vidProgressBar} onInput={progress_seek}></input>
-        </div>
+            <div className={css.invisiblePause} onClick={pauseVid}></div>
+            <div className={btnVis ? css.Hidden : css.rangeCont}>
+                <input type="range" class="progressbar" className={css.range} ref={vidProgressBar} onInput={progress_seek}></input>
+            </div>
+            </div>
         </div>
         </>
     )
