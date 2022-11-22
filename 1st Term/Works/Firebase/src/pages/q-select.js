d6873e79e902c09b7ae278171a658681dd0dd48c
Elite Carlson <elitecarlson@gmail.com>
2022-11-22 06:49:08 +0200

    q-select safety push

--- 1st Term/Works/Firebase/src/pages/q-select.js
+++ 1st Term/Works/Firebase/src/pages/q-select.js
@@ -6,6 +6,7 @@ import { db } from "../firebase-config";
 import { getDoc, doc } from "firebase/firestore";
 import { useState } from "react";
 import PageNotFound from "../components/404";
+import Loading from "../components/loading";
 
 function Qselect(){
     let url = window.location.href;
@@ -19,6 +20,7 @@ function Qselect(){
     const [answer2, setAns2] = useState(null);
     const [answer3, setAns3] = useState(null);
     const [answer4, setAns4] = useState(null);
+    const [page, setPage] = useState("Loading");
 
     if (split.length > 1) {
         const qid = split[1]
@@ -32,37 +34,37 @@ function Qselect(){
                     const docRef = doc(db, 'Questions', qid);
                     const docSnap = await getDoc(docRef)
                         if(docSnap.exists()) {
-                        let data = doc.data();
-                        let qtype = data.Type;
-                        // if (qtype == "selection") {
-                            let video = data.video;
-                            setQuestion(data.question);
-                            setCorrect(data.correct);
-                            setWrong1(data.wrong1);
-                            setWrong2(data.wrong2);
-                            setWrong3(data.wrong3);
+                            let data = docSnap.data();
+                            let qtype = data.Type;
+                            if (qtype == "selection") {
+                                let video = data.video;
+                                setQuestion(data.question);
+                                setCorrect(data.correct);
+                                setWrong1(data.wrong1);
+                                setWrong2(data.wrong2);
+                                setWrong3(data.wrong3);
 
-                            // Fisher Yates shuffle algo
-                            function shuffle(array) {
-                                let newPos,temp;
-                                for(let i = array.length -1; i > 0; i--){
-                                    newPos = Math.floor(Math.random() * (i + 1));
-                                    temp = array[i];
-                                    array[i] = array[newPos];
-                                    array[newPos] = temp;
+                                // Fisher Yates shuffle algo
+                                function shuffle(array) {
+                                    let newPos,temp;
+                                    for(let i = array.length -1; i > 0; i--){
+                                        newPos = Math.floor(Math.random() * (i + 1));
+                                        temp = array[i];
+                                        array[i] = array[newPos];
+                                        array[newPos] = temp;
+                                    }
+                                    return array;
                                 }
-                                return array;
-                            }
 
-                            // Shuffle & Disguise Answers
-                            let ansArray = [correct, wrong1, wrong2, wrong3];
-                            shuffle(ansArray);
-                            console.log(ansArray[0])
-                            return ansArray
-                        // }
+                                // Shuffle & Disguise Answers
+                                let ansArray = [correct, wrong1, wrong2, wrong3];
+                                shuffle(ansArray);
+                                console.log(ansArray[0]);
+                                setPage("Select");
+                            }
                         } else {
                             return(
-                                alert("Document doesn't exist")
+                                setPage("Nonexistant")
                             )
                         }
                 }catch(error){
@@ -76,21 +78,31 @@ function Qselect(){
             <PageNotFound/>
         )
     }
-    return(
-        <>
-        <div className={css.Container}>
-            <Player vid={vid}></Player>
-            <QselectDiv 
-                q={`${question}`} 
-                ans1={`${answer1}`}
-                ans2={`${answer2}`}
-                ans3={`${answer3}`}
-                ans4={`${answer4}`}
-                correct={`${correct}`}
-            ></QselectDiv>
-        </div>
-        </>
-    )
+    if(page == "Loading"){
+        return(
+            <Loading/>
+        )
+    }else if(page == "Select"){
+        return(
+            <>
+            <div className={css.Container}>
+                <Player vid={vid}></Player>
+                <QselectDiv 
+                    q={`${question}`} 
+                    ans1={`${answer1}`}
+                    ans2={`${answer2}`}
+                    ans3={`${answer3}`}
+                    ans4={`${answer4}`}
+                    correct={`${correct}`}
+                ></QselectDiv>
+            </div>
+            </>
+        )
+    }else if(page == "Nonexistant"){
+        return(
+            <PageNotFound/>
+        )
+    }
 }
 
 export default Qselect;
